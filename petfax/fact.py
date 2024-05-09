40fa7fb3124b9b5eb95ed8fb13ba24f6cdf7d11d
Jordan P. Cox <cox.jordan.p@gmail.com>
2024-05-09 18:52:01 -0400

    Redirect works for new facts

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,6 +1,14 @@
-from flask import ( Blueprint, render_template )
+from flask import ( Blueprint, render_template, request, redirect )
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
+
+@bp.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        print(request.form)
+        return redirect('/facts')
+        
+    return 'Thanks for submitting a fun fact!'
 
 @bp.route('/new')
 def new():
